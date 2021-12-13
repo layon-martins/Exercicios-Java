8a6683ed53acfb6553f04086c4aaee6dc6c1ba66
layonFonseca <layon.martins@ufv.br>
2021-12-13 16:57:28 -0300

    exercicios 5 e 6

--- Exercicio5.java
+++ Exercicio5.java
@@ -2,7 +2,32 @@ import java.util.Scanner;
 
 public class Exercicio5 {
     public static void main(String[] args){
+
       Scanner entrada = new Scanner(System.in);
-      entrada.close();
+
+      int dia, mes;
+
+      String meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio","Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+
+      System.out.println("Digite o dia e o número do mês de seu aniversário: ");
+      dia = entrada.nextInt();
+      mes = entrada.nextInt();
+
+     
+      if ((mes==2)&&((dia>0)&&(dia<=29)))
+         System.out.println("Data válida! O mês é "+ meses[mes-1]);
+      else if (((dia>0)&&(dia<=30))&&((mes==4)||(mes==6)||(mes==9)||(mes==11)))
+         System.out.println("Data válida! O mês é "+ meses[mes-1]);
+      else if (((dia>0)&&(dia<=31))&&((mes==3)||(mes==5)||(mes==7)||(mes==8)||(mes==10)||(mes==12)))
+         System.out.println("Data válida! O mês é "+ meses[mes-1]);
+      else
+      System.out.println("Data inválida");
+      
+      entrada.close();  
+
     }
+      
+
 }
+
---
+++ Exercicio6.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        entrada.close();
+    }
+}
